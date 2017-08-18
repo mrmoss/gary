247d90b4dbfadadea522fe707e27c1b00a2564ad
Mike Moss <doomsday.wombats@gmail.com>
2017-08-18 15:03:39 -0800

    added tile load and a kind of save (logs to console)

--- js/tile_maker.js
+++ js/tile_maker.js
@@ -10,20 +10,12 @@ function tile_maker_t(source,width,height)
 	this.place_tpos=null;
 
 	this.mouse_pos={x:0,y:0};
+	this.load_str='';
 }
 
-tile_maker_t.prototype.tile_to_xy_pos=function(tile_x,tile_y)
+tile_maker_t.prototype.tile_tpos_to_xypos=function(tile_x,tile_y)
 {
 	return {x:tile_x*this.width,y:(tile_y+1)*this.height};
-}
-
-tile_maker_t.prototype.get_tile_xy=function(tile_num)
-{
-	if(!this.image.width||!this.image.height)
-		return {x:0,y:0};
-
-	var tiles_per_y=this.get_tile_maker_wh().w;
-	return {x:Math.floor(tile_num%tiles_per_y),y:Math.floor(tile_num/tiles_per_y)};
 }
 
 tile_maker_t.prototype.get_tile_maker_wh=function()
@@ -31,6 +23,39 @@ tile_maker_t.prototype.get_tile_maker_wh=function()
 	if(!this.image.width||!this.image.height)
 		return {w:1,h:1};
 	return {w:Math.floor(this.image.width/this.width),h:Math.floor(this.image.height/this.height)};
+}
+
+tile_maker_t.prototype.load=function(load_str)
+{
+	this.load_str=load_str;
+}
+
+tile_maker_t.prototype.load_from_str=function(load_str)
+{
+	var obj=JSON.parse(load_str);
+	for(var yy=0;yy<obj.length;++yy)
+		for(var xx=0;xx<obj[yy].length;++xx)
+			this.place_tile(obj[yy][xx].tpos,obj[yy][xx].gpos);
+}
+
+tile_maker_t.prototype.save_to_str=function()
+{
+	var save_str='[';
+	for(var yy=0;yy<Math.floor(simulation.canvas.height/this.height);++yy)
+	{
+		save_str+='[';
+		for(var xx=0;xx<Math.floor(simulation.canvas.width/this.width);++xx)
+		{
+			save_str+='{"tpos":{"x":'+this.tiles[yy][xx].tx+',"y":'+this.tiles[yy][xx].ty+'},"gpos":{"x":'+xx+',"y":'+yy+'}}';
+			if(xx+1<Math.floor(simulation.canvas.width/this.width))
+				save_str+=',';
+		}
+		save_str+=']';
+		if(yy+1<Math.floor(simulation.canvas.height/this.height))
+			save_str+=',';
+	}
+	save_str+=']';
+	return save_str;
 }
 
 tile_maker_t.prototype.loop=function(simulation,dt,level)
@@ -44,12 +69,21 @@ tile_maker_t.prototype.loop=function(simulation,dt,level)
 			for(var xx=0;xx<Math.floor(simulation.canvas.width/this.width);++xx)
 				this.tiles[yy].push(new tile_t(-1,-1,null));
 		}
+
+		if(this.load_str)
+		{
+			this.load_from_str(this.load_str);
+			this.load_str='';
+		}
 	}
 
 	if(!this.tiles)
 		return;
 
 	this.shown=simulation.keys_down[kb_control];
+
+	if(this.shown&&simulation.keys_pressed[kb_s])
+		console.log(this.save_to_str());
 
 	this.mouse_pos={x:Math.floor(simulation.mouse_x/this.width),y:Math.floor(simulation.mouse_y/this.height)};
 
@@ -67,28 +101,7 @@ tile_maker_t.prototype.loop=function(simulation,dt,level)
 		}
 		else if(this.place_tpos)
 		{
-			var tile=this.tiles[this.mouse_pos.y][this.mouse_pos.x];
-			tile.tx=this.place_tpos.x;
-			tile.ty=this.place_tpos.y;
-
-			//Removed tile, time to remove the block under it
-			if(!tile.visible()&&tile.block)
-			{
-				var new_blocks=[];
-				for(var ii=0;ii<level.blocks.length;++ii)
-					if(!(level.blocks[ii]===tile.block))
-						new_blocks.push(level.blocks[ii]);
-				level.blocks=new_blocks;
-				tile.block=null;
-			}
-
-			//Added a tile with no block under it, add it
-			else if(tile.visible()&&!tile.block)
-			{
-				var pos=this.tile_to_xy_pos(this.mouse_pos.x,this.mouse_pos.y);
-				tile.block=new block_t(pos.x,pos.y,this.width,this.height);
-				level.blocks.push(tile.block);
-			}
+			this.place_tile(this.place_tpos,this.mouse_pos);
 		}
 	}
 }
@@ -154,6 +167,33 @@ tile_maker_t.prototype.draw=function(simulation)
 			this.mouse_pos.y*this.height,
 			this.width,
 			this.height);
+	}
+}
+
+//place the tile on the tilesheet coordinates tpos at the grid coordinates gpos
+tile_maker_t.prototype.place_tile=function(tpos,gpos)
+{
+	var tile=this.tiles[gpos.y][gpos.x];
+	tile.tx=tpos.x;
+	tile.ty=tpos.y;
+
+	//Removed tile, time to remove the block under it
+	if(!tile.visible()&&tile.block)
+	{
+		var new_blocks=[];
+		for(var ii=0;ii<level.blocks.length;++ii)
+			if(!(level.blocks[ii]===tile.block))
+				new_blocks.push(level.blocks[ii]);
+		level.blocks=new_blocks;
+		tile.block=null;
+	}
+
+	//Added a tile with no block under it, add it
+	else if(tile.visible()&&!tile.block)
+	{
+		var pos=this.tile_tpos_to_xypos(gpos.x,gpos.y);
+		tile.block=new block_t(pos.x,pos.y,this.width,this.height);
+		level.blocks.push(tile.block);
 	}
 }
 
